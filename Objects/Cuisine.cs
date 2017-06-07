014b45460886d5e0043734e46ec0293802e3b023
Andrew Glines and Marilyn Carlin <andrew+marilyn@>
2017-06-07 10:43:24 -0700

    add Save() method to Cuisine.cs

--- Objects/Cuisine.cs
+++ Objects/Cuisine.cs
@@ -70,6 +70,33 @@ namespace Restaurant
       return allCuisines;
     }
 
+    public void Save()
+    {
+      SqlConnection conn = DB.Connection();
+      conn.Open();
+
+      SqlCommand cmd = new SqlCommand("INSERT INTO cuisines (name) OUTPUT INSERTED.id VALUES (@CuisineName);", conn);
+
+      SqlParameter nameParameter = new SqlParameter();
+      nameParameter.ParameterName = "@CuisineName";
+      nameParameter.Value = this.GetName();
+      cmd.Parameters.Add(nameParameter);
+      SqlDataReader rdr = cmd.ExecuteReader();
+
+      while(rdr.Read())
+      {
+        this._id = rdr.GetInt32(0);
+      }
+      if (rdr != null)
+      {
+        rdr.Close();
+      }
+      if (conn != null)
+      {
+        conn.Close();
+      }
+    }
+
     public static void DeleteAll()
       {
         SqlConnection conn = DB.Connection();
