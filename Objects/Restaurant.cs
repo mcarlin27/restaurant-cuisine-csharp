16803fb36b04d796e3684369c449befb04ebac2c
Andrew Glines and Marilyn Carlin <andrew+marilyn@>
2017-06-07 11:26:42 -0700

    instantiate Restaurant constructor.

---
+++ Objects/Restaurant.cs
@@ -0,0 +1,57 @@
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System;
+
+namespace Restaurant
+{
+  public class Restaurant
+  {
+    private int _id;
+    private string _name;
+    private int _cuisineId;
+
+    public Restaurant(string Name, int CuisineId, int Id = 0)
+    {
+      _id = Id;
+      _name = Name;
+      _cuisineId = CuisineId;
+    }
+
+    public int GetId()
+    {
+      return _id;
+    }
+    public string GetName()
+    {
+      return _name;
+    }
+    public void SetName(string newName)
+    {
+      _name = newName;
+    }
+    public int GetCuisineId()
+    {
+      return _cuisineId;
+    }
+    public void SetCuisineId(int newCuisineId)
+    {
+      _cuisineId = newCuisineId;
+    }
+
+    public override bool Equals(System.Object otherRestaurant)
+    {
+      if (!(otherRestaurant is Restaurant))
+      {
+        return false;
+      }
+      else
+      {
+        Restaurant newRestaurant = (Restaurant) otherRestaurant;
+        bool idEquality = (this.GetId() == newRestaurant.GetId());
+        bool nameEquality = (this.GetName() == newRestaurant.GetName());
+        bool cuisineEquality = this.GetCuisineId() == newRestaurant.GetCuisineId();
+        return (idEquality && nameEquality && cuisineEquality);
+      }
+    }
+  }
+}
