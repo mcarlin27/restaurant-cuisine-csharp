2956ccc2cc6c461e19c6a0e019a857cbe3f092db
Andrew Glines and Marilyn Carlin <andrew+marilyn@>
2017-06-07 15:21:19 -0700

    add forms for user input

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -19,6 +19,23 @@ namespace Restaurant
       List<Cuisine> allCuisines = Cuisine.GetAll();
       return View["cuisines.cshtml", allCuisines];
     };
+    Get["/cuisines/new"] = _ => {
+      return View["cuisines_form.cshtml"];
+    };
+    Post["/cuisines/new"] = _ => {
+      Cuisine newCuisine = new Cuisine(Request.Form["cuisine-name"]);
+      newCuisine.Save();
+      return View["success.cshtml"];
+    };
+    Get["/restaurants/new"] = _ => {
+      List<Cuisine> AllCuisines = Cuisine.GetAll();
+      return View["restaurants_form.cshtml", AllCuisines];
+    };
+    Post["/restaurants/new"] = _ => {
+      Restaurant newRestaurant = new Restaurant(Request.Form["restaurant-description"], Request.Form["cuisine-id"]);
+      newRestaurant.Save();
+      return View["success.cshtml"];
+    };
 
 
 
